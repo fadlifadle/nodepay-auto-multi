6c907b00923080a61d06ca7dc9cb4d987a841470
fadle <fadlifadle@gmail.com>
2024-12-29 15:08:35 +0700

    Update run_autoproxy_multi.py

--- run_autoproxy_multi.py
+++ run_autoproxy_multi.py
@@ -5,7 +5,7 @@ import cloudscraper
 from loguru import logger
 from fake_useragent import UserAgent
 import aiohttp
-from banner.banner import banner
+from banners.banner import banner
 from colorama import Fore, Style, init
 
 # Constants
